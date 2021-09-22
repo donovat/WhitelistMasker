178d94f201c7847832dce8c87c5b2a7098788b1c
Tim Donovan <tim_donovan@uk.ibm.com>
2021-09-22 12:39:11 +0100

    Added travis.yml and build_scripts to branch.

---
+++ build_scripts/deploy_image_to_ibm_repo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# @author Tim Donovan IBM Research
+#
+
+echo "Script to push an image built within Travis-ci to IBM's Cloud image repo."
+echo "This script is called by Travis-ci during the deploy stage."
+
+echo "IBM Cloud Region: $IBM_CLOUD_REGION"
+echo "Container Registry Region: $REGISTRY_REGION"
+echo "Container Registry Namespace: $REGISTRY_NAMESPACE"
+echo 
+echo "Image name: $IMAGE_NAME"
+echo "Image tag: $IMAGE_TAG"
+
+############################################################################
+# Log into the IBM Cloud container registry                                          #
+############################################################################
+echo "Checking connected to IBM's Cloud container registry"
+ibmcloud cr login
+if [ $? -ne 0 ]; then
+  echo "Failed to authenticate to IBM Cloud container registry"
+  exit 1
+fi
+ibmcloud cr images
+
+############################################################################
+# Ask Docker to tag the image as latest and with the custom tag            #
+############################################################################
+echo "Tagging the image as $REGISTRY_REGION/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG and $REGISTRY_REGION/$REGISTRY_NAMESPACE/$IMAGE_NAME:latest"
+docker tag $IMAGE_NAME:$IMAGE_TAG $REGISTRY_REGION/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG
+docker tag $IMAGE_NAME:$IMAGE_TAG $REGISTRY_REGION/$REGISTRY_NAMESPACE/$IMAGE_NAME:latest
+
+############################################################################
+# Push the image                                                           #
+############################################################################
+echo "Pushing image to registry"
+docker push $REGISTRY_REGION/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG
+docker push $REGISTRY_REGION/$REGISTRY_NAMESPACE/$IMAGE_NAME:latest
+############################################################################
+# end script                                                               #
+############################################################################
+
