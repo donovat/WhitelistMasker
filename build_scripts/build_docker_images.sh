c88566f10ffd5ea49f82c7c5683a4377feb78a76
Tim Donovan <tim_donovan@uk.ibm.com>
2021-09-27 13:10:39 +0100

    Multiple changes to the travis-ci script, fix docker bug, remove after_success

--- build_scripts/build_docker_images.sh
+++ build_scripts/build_docker_images.sh
@@ -2,7 +2,7 @@
 
 # @author Tim Donovan IBM Research
 #
-set -x
+set -ev
 
 echo "This is the script to build one or more Docker Images"
 echo "related to the WhitelistMasker"
@@ -10,6 +10,11 @@ echo "related to the WhitelistMasker"
 # Check if Masker jar file has been built into target directory
 if [[ ! -f Masker/target/Masker-1.1.4-jar-with-dependencies.jar ]]; then
    echo "Missing Masker jar file in Masker/target, cannot continue with build."
+   exit 1
+fi
+# Check if MaskWebServices war file present
+if [[ ! -f target/MaskWebServices-1.1.4.war ]]; then
+   echo "Missing MaskWebServices war file in target, cannot continue with build."
    exit 1
 fi
 
@@ -21,8 +26,8 @@ echo "Image Name: ${IMAGE_NAME}"
 echo "Image Tag: $_imageTag"
 
 echo "Note: At the moment all images are only pushed to the local docker repository."
-# build docker image
-DOCKER_BUILDKIT=0 docker build --no-cache --progress plain --tag _ImageName:$_imageTag  -f /MaskWebServices/Dockerfile_MaskWebServices .
+# build docker image ( original flag --progress plain errors )
+DOCKER_BUILDKIT=0 docker build --no-cache --tag _ImageName:$_imageTag  -f MaskWebServices/Dockerfile_MaskWebServices .
 if [ $? -ne 0 ]; then
    echo "Failed to build image."
    exit 1
