c342affdade15d41fdd41df7096afe4bc0906f41
Tim Donovan <tim_donovan@uk.ibm.com>
2021-11-03 18:37:06 +0000

    changes to install ibm cloud cli script, due to ssl errors
    Update

--- build_scripts/ibm_cloud_cli_tools_install.sh
+++ build_scripts/ibm_cloud_cli_tools_install.sh
@@ -3,12 +3,11 @@
 # @author Tim Donovan IBM Research
 #
 # set script to fail if any command does not return '0'
-<<<<<<< HEAD
 
 # Note: This is my version of a script to install the IBM Cloud CLI tools
 # rather than the DEPRICATED installer: https://ibm.biz/idt-installer 
 set -e 
-set +x
+set -x
 
 ############################################################################
 # Download and install the IBM Cloud CLI tool.                             #
@@ -21,10 +20,14 @@ curl -sSL https://clis.cloud.ibm.com/install/linux | sh
 
 ibmcloud version
 ibmcloud plugin list
-=======
 ############################################################################
 echo "Install IBM Cloud CLI"
 curl --version
 curl --insecure -fsSL https://clis.cloud.ibm.com/install/linux | sh
 
+ibmcloud --help
+curl -ksSL https://clis.cloud.ibm.com/install/linux | bash
+echo "Try a gain"
+curl -ksSL -o install.sh https://clis.cloud.ibm.com/install/linux
+cat ./install.sh
 ibmcloud --help
