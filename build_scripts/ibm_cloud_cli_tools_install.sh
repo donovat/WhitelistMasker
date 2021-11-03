3913703af948d0577ccf33cd720d0fe3584c513c
Tim Donovan <tim_donovan@uk.ibm.com>
2021-11-03 18:27:38 +0000

    changes to install ibm cloud cli script, due to ssl errors

--- build_scripts/ibm_cloud_cli_tools_install.sh
+++ build_scripts/ibm_cloud_cli_tools_install.sh
@@ -6,15 +6,13 @@
 set -e 
 set +x
 
-ls -al /etc/ssl/certs/ca-certificates.crt 
-ls -al /etc/ssl/certs/
-
 ############################################################################
 # Download and install the IBM Cloud CLI tool.                             #
 # Documentation on details can be found here:                              #
 #    https://cloud.ibm.com/docs/cli/index.html                             #
 ############################################################################
 echo "Install IBM Cloud CLI"
+curl --version
 curl --insecure -fsSL https://clis.cloud.ibm.com/install/linux | sh
 
 ibmcloud --help
