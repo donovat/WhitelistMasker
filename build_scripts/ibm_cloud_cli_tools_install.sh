ac2c003eba910ce2ad76e86a465d985b3f83e873
Tim Donovan <tim_donovan@uk.ibm.com>
2021-11-04 17:58:33 +0000

    Fixed errors with curl & ssl. Allowing new image to be built and pushed to IBM Cloud repo

--- build_scripts/ibm_cloud_cli_tools_install.sh
+++ build_scripts/ibm_cloud_cli_tools_install.sh
@@ -24,9 +24,7 @@ echo "Install IBM Cloud CLI"
 curl --version
 curl --insecure -fsSL https://clis.cloud.ibm.com/install/linux | sh
 
-ibmcloud --help
 curl -ksSL https://clis.cloud.ibm.com/install/linux | bash
 echo "Try a gain"
 curl -ksSL -o install.sh https://clis.cloud.ibm.com/install/linux
 cat ./install.sh
-ibmcloud --help
